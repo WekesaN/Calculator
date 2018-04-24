c8057d37e7d8dce8050c953d5e0aa22192e1abf3
WekesaN <isaacnyongesa2048@gmail.com>
2018-04-24 22:56:49 +0300

    remove the semi colon

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
 var add = function(number1, number2){ return (number1) + number2}
 var number1 = parseInt(prompt("enter the first number"));
 var number2 = parseInt(prompt("enter the second number"));
-alert("The sum is" + :add(number1,number2));
+alert("The sum is" + add(number1,number2));
