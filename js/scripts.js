8f3d6800a7b54599df4e8468505ed62716ea51a3
WekesaN <isaacnyongesa2048@gmail.com>
2018-04-24 22:09:32 +0300

    add the branch added

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-var add = function(number1, number2){ return number1 + number2}
+var add = function(number1, number2){ return (number1) + number2}
 var number1 = parseInt(prompt("enter the first number"));
 var number2 = parseInt(prompt("enter the second number"));
 alert(add(number1,number2));
